03d5735129d5c5634c8227650e081d6e1ff62aff
Sabareesh <SabareeshSS@users.noreply.github.com>
2017-09-27 14:37:56 +0530

    Update contact-form.php
    New set of fields

--- contact-form/contact-form.php
+++ contact-form/contact-form.php
@@ -1,12 +1,13 @@
 <?php
 if(isset($_POST['submit'])) 
 {
+
 $message=
-'Name		:	'.$_POST['fullname'].'<br />
-Mobile		:	'.$_POST['phone'].'<br />
-Email		:	'.$_POST['emailid'].'<br />
-Attendees	:	'.$_POST['subject'].'<br />
-Greetings	:	'.$_POST['comments'].'<br />
+'Name of the Guest	:	'.$_POST['name'].'<br />
+Phone Number 		:	'.$_POST['phone'].'<br />
+Email Address		:	'.$_POST['email'].'<br />
+Number of Guests	:	'.$_POST['dropdown'].'<br />
+Greetings			:	'.$_POST['message'].'<br />
 ';
     require "phpmailer/class.phpmailer.php"; //include phpmailer class
       
@@ -14,30 +15,30 @@ Greetings	:	'.$_POST['comments'].'<br />
     $mail = new PHPMailer();  
       
     // Set up SMTP  
-    $mail->IsSMTP();                // Sets up a SMTP connection  
-    $mail->SMTPAuth = true;         // Connection with the SMTP does require authorization    
-    $mail->SMTPSecure = 'ssl';     // Connect using a TLS connection  
-    $mail->Host = "smtp.gmail.com";  //Gmail SMTP server address
-    $mail->Port = 465;  //Gmail SMTP port Options: Port 25, 465, or 587
+    $mail->IsSMTP();               	 	// Sets up a SMTP connection  
+    $mail->SMTPAuth = true;         	// Connection with the SMTP does require authorization    
+    $mail->SMTPSecure = 'ssl';     		// Connect using a TLS connection  
+    $mail->Host = "smtp.gmail.com";  	// Gmail SMTP server address
+    $mail->Port = 465;  				// Gmail SMTP port Options: Port 25, 465, or 587
     $mail->Encoding = '7bit';
     
     // Authentication  
-    $mail->Username   = "sawedssha@gmail.com"; // Your full Gmail address
-    $mail->Password   = "feb052018"; // Your Gmail password
+    $mail->Username   = "sawedssha@gmail.com"; 	// Your full Gmail address
+    $mail->Password   = "febx052018"; 			// Your Gmail password
       
     // Compose
-    $mail->SetFrom($_POST['emailid'], $_POST['fullname']);
-    $mail->AddReplyTo($_POST['emailid'], $_POST['fullname']);
+    $mail->SetFrom($_POST['email'], $_POST['name']);
+    $mail->AddReplyTo($_POST['email'], $_POST['name']);
     $mail->Subject = "RSVP Received: A New Attendee";      // Subject (which isn't required)  
     $mail->MsgHTML($message);
  
     // Send To  
     $mail->AddAddress("sssabrsh@gmail.com", "Sabareesh"); // Where to send it - Recipient
     $result = $mail->Send();		// Send!  
-	$message01 = $result ? 'Thank you so much for accepting the invitation!' : 'Sorry for the inconvenience! Please do RSVP later.';      
+	$message01 = $result ? 'Thank you so much for accepting the invitation!' : 'Sorry for the inconvenience! Please do RSVP later.';
 	echo $message01;
 	unset($mail);
 }
-	//header("Location: success.html");
-	//exit;
+	header("Location: ../success.html");
+	exit;
 ?>
